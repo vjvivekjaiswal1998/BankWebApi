662a9026436529a49bb49bbb7159e3a1c8e24d01
Vivek Jaiswal <vivek.jaiswal@1rivet.local>
2020-02-20 03:50:14 -0800

    Add : Added New project with webapi

---
+++ Bank/Controllers/BankController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using BankManagement.BLL;
+using BankManagement.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Controllers
+{
+    public class BankController : Controller
+    {
+        private readonly IBank _bank;
+        public BankController(IBank bank)
+        {
+            _bank = bank;
+        }
+
+        public IActionResult Index()
+        {
+        //    ViewData["Message"] = "Welcome";
+            ViewBag.Welcome = "Welcome to this website . This website is for Bank Management system";
+            ViewData["Message"] = "Hello Vivek R Jaiswal!";
+            return View();
+        }
+        BankDetail bankDetail = new BankDetail();
+
+        //To Show All Bank Detail.
+        [HttpGet]
+        public IActionResult ShowAllBankDetail()
+        {
+            List<BankDetail> BankList = new List<BankDetail>();
+            BankList = _bank.ShowAllBankDetail();
+
+            return View(BankList);
+
+        }
+        // To Show Single Bank Detail.
+        [HttpGet]
+        public IActionResult GetBankDetail(int accountNumber)
+        {
+            
+            bankDetail = _bank.GetSingleBankAccountDetail(accountNumber);
+
+            return View(bankDetail);
+           
+        }
+        //To Insert The Data In Bank Data Base.
+        //[httpGet]
+
+        public IActionResult Add(BankDetail bankDetail)
+        {
+            _bank.AddBankDetail(bankDetail);
+            return View("AddBankDetail");
+        }
+        public IActionResult AddBankDetail()
+        {        
+            return View();
+        }
+        //To Update The Data In Bank Data Base.
+        //[HttpPut]
+        public IActionResult UpdateBankDetail(int accountNumber, string CustomerEmail , string CustomerPhoneNumber)
+        {
+            _bank.UpdatebankDetail(accountNumber , CustomerEmail , CustomerPhoneNumber);
+            return View(bankDetail);        
+        }
+        //To Delete The Data In BanK Data Base.
+    //    [HttpDelete]
+        public IActionResult DeleteBankDetail(int accountNumber)
+        {
+            _bank.DeleteBankDetail(accountNumber);
+            return View(bankDetail);
+          
+        }
+     
+    }
+}
---
+++ BankWebApi/Controllers/BankController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using BankManagement.BLL;
+using BankManagement.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankController : ControllerBase
+    {
+
+        private readonly IBank _bank;
+        BankDetail bankDetail = new BankDetail();
+        public BankController(IBank bank)
+        {
+            _bank = bank;
+        }
+      
+        //To Show All Bank Detail.
+        [HttpGet]
+        public ActionResult<List<string>> ShowAllBankDetail()
+        {
+            List<BankDetail> bankDetail = _bank.ShowAllBankDetail();
+            List<string> all = new List<string>();
+            foreach (BankDetail bankDetails in bankDetail)
+            {
+                System.IO.StringWriter writer = new System.IO.StringWriter();
+                new XmlSerializer(bankDetail.GetType()).Serialize(writer, bankDetail);
+                string xml = writer.ToString();
+                all.Add(xml);
+            }
+            return all;
+
+        }
+        // To Show Single Bank Detail.
+        [HttpGet("{accountNumber}")]
+        public string GetSingleBankDetail(int accountNumber)
+        {
+            BankDetail bankDetail = _bank.GetSingleBankAccountDetail(accountNumber);
+            System.IO.StringWriter writer = new System.IO.StringWriter();
+            new XmlSerializer(bankDetail.GetType()).Serialize(writer, bankDetail);
+            string xml = writer.ToString();
+            return xml;
+
+        }
+        //// To Show Single Bank Detail.
+        //[HttpGet("{accountNumber}")]
+        //public ActionResult<BankDetail> GetSingleBankDetail(int accountNumber)
+        //{
+        //    BankDetail bankDetail = _bank.GetSingleBankAccountDetail(accountNumber);
+        //    return bankDetail;
+
+        //}
+        //To Insert The Data In Bank Data Base.
+        //[httpGet]
+        [HttpPost]
+        public BankDetail Add([FromBody]BankDetail bankDetail)
+        {       
+            if (bankDetail != null)
+            {
+                _bank.AddBankDetail(bankDetail);
+            }
+            return bankDetail;
+        }
+        //To Update The Data In Bank Data Base.
+        [HttpPut("{accountNumber}")]
+        public string  UpdateBankDetail(int accountNumber, [FromBody]BankDetail bankDetail)
+        {
+            _bank.UpdatebankDetail(accountNumber,bankDetail.CustomerEmail,bankDetail.CustomerPhoneNumber);
+            return "update successfully";
+        }
+        //To Delete The Data In BanK Data Base.
+        [HttpDelete("{accountNumber}")]
+        public string DeleteBankDetail(int accountNumber)
+        {
+            _bank.DeleteBankDetail(accountNumber);
+
+            return "Deleted successfully";
+        }
+     
+
+    }
+}
